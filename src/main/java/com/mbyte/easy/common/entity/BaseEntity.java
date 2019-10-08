81cbc0b54424774918a2c47c741054bcf5bc6ecb
申劭明 <42922512+shenshaoming@users.noreply.github.com>
2019-10-08 12:58:51 +0800

    shenshaoming

--- src/main/java/com/mbyte/easy/common/entity/BaseEntity.java
+++ src/main/java/com/mbyte/easy/common/entity/BaseEntity.java
@@ -12,6 +12,7 @@ import lombok.Data;
 public class BaseEntity {
     /**
      * 主键id,数据库中必须有id字段,且id字段为主键自增的bigint类型
+     * @TableId 指定该属性为自增类型
      */
     @TableId(type = IdType.AUTO,value = "id")
     private long id;
